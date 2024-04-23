214c0c3827b3dddb26f59ecdcc3cdecefc22420f
raghav bhardwaj <raghavmanc@gmail.com.com>
2024-04-23 13:31:57 +0530

    added Footer and interface Props for the center body

--- frontend/src/layouts/Layout.tsx
+++ frontend/src/layouts/Layout.tsx
@@ -1,13 +1,23 @@
+import Footer from "../components/Footer"
 import Header from "../components/Header"
 import Hero from "../components/Hero"
 
+// describe the props that layout component expects
+interface Props{
+  children: React.ReactNode;
+}
 
-const Layout = () => {
+
+const Layout = ({children}: Props) => {
   return (
     //Aligns elements in a columns, min-h makes sure header is at top and footer and bottom
     <div className="flex flex-col min-h-screen">
         <Header />
         <Hero />
+        <div className="container mx-auto py-10 flex-1">
+          {children}
+        </div>
+        <Footer/>
     </div> 
   )
 }
